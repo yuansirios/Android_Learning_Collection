38ef4b88e1c2cb60f3d5aa06310792cfc8fc65d9
yuan <695580111@qq.com>
2019-05-23 10:36:28 +0800

    完善动态反射事件

--- app/src/main/java/com/yuan/android_learning_collection/MainActivity.java
+++ app/src/main/java/com/yuan/android_learning_collection/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -17,7 +16,6 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 import android.widget.ListView;
 import android.widget.AdapterView;
 
@@ -45,25 +43,25 @@ public class MainActivity extends AppCompatActivity {
         //1、数据源
         {
 
-            ListModel model = new ListModel("[1]LinearLayout","线性布局，示例演示","");
+            ListModel model = new ListModel("[1]LinearLayout","线性布局，示例演示","LinearLayout");
             dataArr.add(model);
 
-            ListModel model1 = new ListModel("[2]RelativeLayout","相对布局，示例演示","");
+            ListModel model1 = new ListModel("[2]RelativeLayout","相对布局，示例演示","RelativeLayout");
             dataArr.add(model1);
 
-            ListModel model2 = new ListModel("[3]TableLayout","表格布局，示例演示","");
+            ListModel model2 = new ListModel("[3]TableLayout","表格布局，示例演示","TableLayout");
             dataArr.add(model2);
 
-            ListModel model3 = new ListModel("[4]FrameLayout","帧布局，示例演示","");
+            ListModel model3 = new ListModel("[4]FrameLayout","帧布局，示例演示","FrameLayout");
             dataArr.add(model3);
 
-            ListModel model4 = new ListModel("[5]GridLayout","网格布局，示例演示","");
+            ListModel model4 = new ListModel("[5]GridLayout","网格布局，示例演示","GridLayout");
             dataArr.add(model4);
 
-            ListModel model5 = new ListModel("[6]AbsoluteLayout","绝对布局，示例演示","");
+            ListModel model5 = new ListModel("[6]AbsoluteLayout","绝对布局，示例演示","AbsoluteLayout");
             dataArr.add(model5);
 
-            ListModel model6 = new ListModel("[7]ListView动态行高","示例演示","");
+            ListModel model6 = new ListModel("[7]ListView动态行高","示例演示","dynHeight");
             dataArr.add(model6);
         }
 
@@ -104,79 +102,63 @@ public class MainActivity extends AppCompatActivity {
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    switch (position) {
-                        case 1:
-                            this.LinearLayoutTest();
-                            break;
-                        case 2:
-                            this.RelativeLayout();
-                            break;
-                        case 3:
-                            this.TableLayout();
-                            break;
-                        case 4:
-                            this.FrameLayout();
-                            break;
-                        case 5:
-                            this.GridLayout();
-                            break;
-                        case 6:
-                            this.AbsoluteLayout();
-                            break;
-                        case 7:
-                            this.dynHeight();
-                            break;
 
-                            default:
-                                ListModel model = dataArr.get(position-1);
-                                Toast.makeText(MainActivity.this, "点击了" + model.getTitle(), Toast.LENGTH_SHORT).show();
-                                break;
-
+                    try {
+                        ListModel model = dataArr.get(position-1);
+                        Method method = this.getClass().getMethod(model.getEvent());
+                        method.invoke(this);
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
                     }
                 }
 
-                private void LinearLayoutTest() {
+                public void LinearLayout() {
                     System.out.println("线性布局");
-                    Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                    Intent intent = new Intent(MainActivity.this,LinearLayoutActivity.class);
                     startActivity(intent);
                 }
 
-                private void RelativeLayout() {
+                public void RelativeLayout() {
                     System.out.println("相对布局");
                     Intent intent = new Intent(MainActivity.this, RelativeLayoutActivity.class);
                     startActivity(intent);
                 }
 
-                private void TableLayout() {
+                public void TableLayout() {
                     System.out.println("表格布局");
                     Intent intent = new Intent(MainActivity.this, TableLayoutActivity.class);
                     startActivity(intent);
                 }
 
-                private void FrameLayout() {
+                public void FrameLayout() {
                     System.out.println("帧布局");
                     Intent intent = new Intent(MainActivity.this, FrameLayoutActivity.class);
                     startActivity(intent);
                 }
 
-                private void GridLayout() {
+                public void GridLayout() {
                     System.out.println("网格布局");
                     Intent intent = new Intent(MainActivity.this, GridLayoutActivity.class);
                     startActivity(intent);
                 }
 
-                private void AbsoluteLayout() {
+                public void AbsoluteLayout() {
                     System.out.println("绝对布局");
                     Intent intent = new Intent(MainActivity.this, AbsolutLayoutActivity.class);
                     startActivity(intent);
                 }
 
-                private void dynHeight() {
+                public void dynHeight() {
                     System.out.println("动态行高");
                     Intent intent = new Intent(MainActivity.this, DynHeightListActivity.class);
                     startActivity(intent);
                 }
-
             });
         }
     }
